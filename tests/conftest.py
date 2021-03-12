37f48783ea737825d1fd127a3d9c40ef13b8a37a
Iulian Udrea <iulian.udrea@gmail.com>
2021-03-12 20:45:52 +0000

    Add pytest fixtures for expected positions after executing commands

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,29 @@ def expected_grid():
         (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5),
         (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5)
     ]
+
+
+@pytest.fixture
+def rover_landing_position():
+    return (0, 0, 'E')
+
+
+@pytest.fixture
+def expected_pos_after_forwards_cmd():
+    return (1, 0, 'E')
+
+
+@pytest.fixture
+def expected_pos_after_backwards_cmd():
+    return (3, 0, 'E')
+
+
+@pytest.fixture
+def expected_pos_after_left_cmd():
+    return (0, 0, 'N')
+
+
+@pytest.fixture
+def expected_pos_after_right_cmd():
+    return (0, 0, 'S')
+

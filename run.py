d8bc6544512392b77c82f47a61949ca33f114669
Iulian Udrea <iulian.udrea@gmail.com>
2021-03-12 23:03:17 +0000

    Minor tweaks to run.py

--- run.py
+++ run.py
@@ -4,13 +4,14 @@ from rover.rover import Rover
 if __name__ == '__main__':
     grid_size = (4, 6)
     landing_position = (0, 0, 'E')
-    # obstacles = [(1, 4), (3, 1)]
-    obstacles = [(1, 4)]
+    obstacles = [(1, 4), (3, 1)]
+    # obstacles = [(1, 4)]
     journey_commands = [
         'F', 'F', 'L', 'F', 'R', 'F', 'B', 'L', 'F', 'F', 'F', 'F'
     ]
+
     rover = Rover(grid_size=grid_size, landing_position=landing_position)
     
     journey_history = rover.begin_journey(journey_commands, obstacles)
     
-    print(journey_history)
+    print(f'Journey history:\n{journey_history}')

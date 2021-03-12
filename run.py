d367ba4f6cc55a8b72e8385f88e7aa9444c1fc7b
Iulian Udrea <iulian.udrea@gmail.com>
2021-03-12 22:49:25 +0000

    Update run.py

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ from rover.rover import Rover
 
 if __name__ == '__main__':
     grid_size = (4, 6)
-    landing_position = (1, 1, 'E')
-    journey_commands = ['F', 'F', 'L', 'F', 'R', 'F', 'B', 'L', 'F', 'F', 'F', 'F']
-    
+    landing_position = (0, 0, 'E')
+    # obstacles = [(1, 4), (3, 1)]
+    obstacles = [(1, 4)]
+    journey_commands = [
+        'F', 'F', 'L', 'F', 'R', 'F', 'B', 'L', 'F', 'F', 'F', 'F'
+    ]
     rover = Rover(grid_size=grid_size, landing_position=landing_position)
-    print(f'Grid:\n{rover.grid}')
-
-    journey_history = rover.begin_journey(journey_commands)
+    
+    journey_history = rover.begin_journey(journey_commands, obstacles)
     
     print(journey_history)

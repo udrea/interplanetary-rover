316c4353b044829baf941f98a53b900d53ceef45
Iulian Udrea <iulian.udrea@gmail.com>
2021-03-12 21:01:52 +0000

    Write tests for rest of commands: B, L and R

--- tests/test_rover.py
+++ tests/test_rover.py
@@ -14,4 +14,30 @@ def test_calculate_new_pos_vector_forwards(
 ):
     actual = calculate_new_pos_vector(rover_landing_position, forwards_command)
     assert expected_pos_after_forwards_cmd == actual
-    
+
+
+def test_calculate_new_pos_vector_backwards(
+    rover_landing_position,
+    backwards_command,
+    expected_pos_after_backwards_cmd
+):
+    actual = calculate_new_pos_vector(rover_landing_position, backwards_command)
+    assert expected_pos_after_backwards_cmd == actual
+
+
+def test_calculate_new_pos_vector_left_turn(
+    rover_landing_position,
+    left_turn_command,
+    expected_pos_after_left_cmd
+):
+    actual = calculate_new_pos_vector(rover_landing_position, left_turn_command)
+    assert expected_pos_after_left_cmd == actual
+
+
+def test_calculate_new_pos_vector_right_turn(
+    rover_landing_position,
+    right_turn_command,
+    expected_pos_after_right_cmd
+):
+    actual = calculate_new_pos_vector(rover_landing_position, right_turn_command)
+    assert expected_pos_after_right_cmd == actual
